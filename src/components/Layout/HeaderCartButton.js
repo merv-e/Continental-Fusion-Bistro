5ca5c10c96d25aad1e04a09e060c254e14f3224f
merv-e <34655454+merv-e@users.noreply.github.com>
2023-06-27 14:31:19 +0200

    js reduce method is being used. It's a bit complicated.

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,16 @@
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = props => {
+  
+  const ctx = useContext(CartContext);
+
+  const numberOfCartItems = ctx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
     <span className={classes.icon}>
@@ -9,7 +18,7 @@ const HeaderCartButton = props => {
     </span>
     <span>Your Cart</span>
     <span className={classes.badge}>
-        3
+        {numberOfCartItems}
     </span>
     </button>
   )
