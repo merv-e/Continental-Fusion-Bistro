ebe88b0f7d6f61fc9edd51b19dfbf20202b1869f
merv-e <34655454+merv-e@users.noreply.github.com>
2023-07-03 13:05:58 +0200

    cute little bump effect on the button when adding or removing an item :)

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,39 @@
+import { useContext, useEffect, useState } from 'react';
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
-import { useContext } from 'react';
 import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = props => {
   
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
   const ctx = useContext(CartContext);
 
-  const numberOfCartItems = ctx.items.reduce((curNumber, item) => {
+  const {items} = ctx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  useEffect(()=> {
+    if(items.length ===0 ) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(()=> {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () =>  {
+      clearTimeout(timer);
+    }
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
     <span className={classes.icon}>
         <CartIcon />
     </span>
