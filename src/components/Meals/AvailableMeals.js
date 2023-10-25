b3bd57ce3e7eec8e25cc439a196947bb574b061a
merv-e <34655454+merv-e@users.noreply.github.com>
2023-10-25 13:02:03 +0200

    error messages and loading messages are added.

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -4,14 +4,13 @@ import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
-
   const [meals, setMeals] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
   const fetchMeals = useCallback(async () => {
-     setIsLoading(true);
-     setError(null);
+    setIsLoading(true);
+    setError(null);
 
     try {
       const response = await fetch(
@@ -19,26 +18,28 @@ const AvailableMeals = () => {
       );
 
       if (!response.ok) {
-        throw new Error("Something went wrong!");
+        throw new Error("Something went wrong!!");
       }
 
-       const data = await response.json();
-       console.log(data); 
+      const data = await response.json(); //this will return an object
+      //  console.log(data);
 
-       const fetchedMeals = [];
+      const fetchedMeals = []; //we want it inside an array.
 
-       for (const key in data) {
-         fetchedMeals.push({
-           id: key,
-           name: data[key].name,
-           price: data[key].price,
-           description: data[key].description,
-         });
-       }
+      for (const key in data) {
+        //loop through the object
+        fetchedMeals.push({
+          id: key,
+          name: data[key].name,
+          price: data[key].price,
+          description: data[key].description,
+        });
+      }
 
-       setMeals(fetchedMeals);
-
+      setMeals(fetchedMeals);
+      setIsLoading(false);
     } catch (error) {
+      setIsLoading(false);
       setError(error.message);
     }
   }, []);
@@ -46,6 +47,22 @@ const AvailableMeals = () => {
   useEffect(() => {
     fetchMeals();
   }, [fetchMeals]);
+
+  if (isLoading) {
+    return (
+      <section>
+        <h2 className={classes.loading}>Loading...</h2>
+      </section>
+    );
+  }
+
+  if (error) {
+    return (
+      <section className={classes.error}>
+        <h2>{error}</h2>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
