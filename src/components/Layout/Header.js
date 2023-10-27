e5053384531a5973d00598459ef2e6b9278b1d11
merv-e <34655454+merv-e@users.noreply.github.com>
2023-10-27 17:41:57 +0200

    minor changes

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,7 +2,7 @@ import { Fragment } from 'react';
 import classes from './Header.module.css';
 import meals from '../../assets/meals.jpg'
 import HeaderCart from './HeaderCart';
-import icon from '../../../src/assets/logo-android-chrome-192.png'
+import icon from '../../../src/assets/logo-android-chrome-192.png';
 
 const Header = props => {
   return (
@@ -10,7 +10,7 @@ const Header = props => {
       <header className={classes.header}>
       <div className={classes["header-icon-and-name"]}>
         <img src={icon} alt="Icon of the website" className={classes.icon}/>
-        <h1>Continental Fusion Bistro</h1>
+        <h1 className={classes["restaurant-name"]} >Continental Fusion Bistro</h1>
       </div>
         <HeaderCart onClick={props.onShowCart} />
       </header>
