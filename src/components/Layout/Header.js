e9f1bad064cf40bc77a9a3b18781949441f3d4bf
merv-e <34655454+merv-e@users.noreply.github.com>
2023-11-07 16:49:00 +0100

    - styling of the menu element in header. - plus readme updated.

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,6 +7,22 @@ import OrderHistory from "./OrderHistory";
 
 const Header = (props) => {
   const [isScreenSmall, setIsScreenSmall] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const openMenu = isMenuOpen && (
+    <div className={classes["hidden-menu"]}>
+      <button onClick={() => setIsMenuOpen(false)}>Close</button>
+      <button onClick={props.onShowCart}>Your Cart</button>
+      <button>
+        {/*  onClick={props.onShowOrder} */}
+        Order History
+      </button>
+    </div>
+  );
+
+  const changeHeaderClass = `${classes.header} ${
+    isMenuOpen && isScreenSmall ? classes["fullscreen-menu"] : ""
+  }`;
 
   useEffect(() => {
     const handleResize = () => {
@@ -22,28 +38,35 @@ const Header = (props) => {
 
   return (
     <Fragment>
-      <header className={classes.header}>
-        <div className={classes["header-icon-and-name"]}>
-          <img src={icon} alt="Icon of the website" className={classes.icon} />
-          <h1 className={classes["restaurant-name"]}>
-            Continental Fusion Bistro
-          </h1>
-        </div>
-        <div className={classes["order-info"]}>
-          {!isScreenSmall ? (
-            <>
-              <HeaderCart onShowCart={props.onShowCart} />
-              <OrderHistory />
-            </>
-          ) : (
-            <div className={classes.menu}>
-              <button className={classes["menu-button"]}>
-                <span>&#9776;</span>
-                
+      <header className={changeHeaderClass}>
+        <div className={classes.navbar}>
+          <div className={classes["header-icon-and-name"]}>
+            <img
+              src={icon}
+              alt="Icon of the website"
+              className={classes.icon}
+            />
+            <h1 className={classes["restaurant-name"]}>
+              Continental Fusion Bistro
+            </h1>
+          </div>
+          <div className={classes["order-info"]}>
+            {!isScreenSmall ? (
+              <>
+                <HeaderCart onShowCart={props.onShowCart} />
+                <OrderHistory />
+              </>
+            ) : (
+              <button
+                className={classes["menu-button"]}
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+              >
+                &#9776;
               </button>
-            </div>
-          )}
+            )}
+          </div>
         </div>
+        {isScreenSmall && isMenuOpen && openMenu}
       </header>
 
       <div className={classes["main-image"]}>
